3d7d31bda095f66ba7056ef6cae2a4c19f473559
Joseph Palermo <jpalermo@pivotal.io>
2021-08-14 16:16:55 -0700

    Upgrade from go dep to go mod

--- ci/tasks/test-integration.sh
+++ ci/tasks/test-integration.sh
@@ -4,20 +4,7 @@ set -ex
 
 source ~/.bashrc
 
-export GOPATH=$(pwd)/gopath
-export GOBIN=$GOPATH/gobin
-export PATH=/usr/local/go/bin:$GOPATH:$GOBIN:$PATH
-
-cd gopath/src/github.com/cloudfoundry/usn-resource
-
-echo -e "\n Cleaning..."
-go clean -r .
-
-echo -e "\n Installing ginkgo..."
-go get -u github.com/onsi/ginkgo/ginkgo
-
-echo -e "\n Installing gomega..."
-go get -u github.com/onsi/gomega/...
+cd usn-resource
 
 echo -e "\n Running unit tests..."
-ginkgo -r $race -keepGoing -trace $@
+go run github.com/onsi/ginkgo/ginkgo -r $race -keepGoing -trace $@
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/cloudfoundry/usn-resource
+
+go 1.16
+
+require (
+	github.com/PuerkitoBio/goquery v1.4.1
+	github.com/andybalholm/cascadia v1.0.0 // indirect
+	github.com/mmcdole/gofeed v1.0.0-beta2
+	github.com/mmcdole/goxpp v0.0.0-20170720115402-77e4a51a73ed // indirect
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.15.0
+	github.com/stretchr/testify v1.7.0 // indirect
+)
