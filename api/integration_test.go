3771ea5c96e17ad309a10934854108241128c1c6
Joseph Palermo <jpalermo@pivotal.io>
2021-08-14 16:17:19 -0700

    Fix bug in tests when there is a feed item with no CVE

--- api/integration_test.go
+++ api/integration_test.go
@@ -12,20 +12,20 @@ var _ = Describe("CVE", func() {
 		fp := gofeed.NewParser()
 		feed, _ := fp.ParseURL("https://usn.ubuntu.com/usn/rss.xml")
 		Expect(feed.Items).To(HaveLen(10))
+
+		foundRealPriority := false
 		for _, item := range feed.Items {
 			usn := api.USNFromFeed(item)
 			Expect(len(usn.Releases())).Should(BeNumerically(">", 0), "No releases were found for any usns")
 
-			foundRealPriority := false
 			for _, priority := range usn.CVEs().Priorities() {
 				if priority != "unknown" {
 					foundRealPriority = true
 				}
 				Expect(priority).To(MatchRegexp(`^(?i)(critical|high|medium|low|negligible|untriaged|unknown)$`))
 			}
-
-			Expect(foundRealPriority).To(BeTrue(), "Priority parsing seems to be broken, expected to find at least one real cve priority in 10 rss feed usns")
 		}
+		Expect(foundRealPriority).To(BeTrue(), "Priority parsing seems to be broken, expected to find at least one real cve priority in 10 rss feed usns")
 	})
 
 	It("returns an unknown priority if CVE cannot be found", func() {
@@ -33,13 +33,13 @@ var _ = Describe("CVE", func() {
 		Expect(cve.Priority()).To(Equal("unknown"))
 	})
 
-	It("parses the priority for a single CVE", func(){
+	It("parses the priority for a single CVE", func() {
 		url := "https://ubuntu.com/security/CVE-2020-15257"
 		cve := api.CVE{URL: url}
 		Expect(cve.Priority()).To(Equal("medium"))
 	})
 
-	It("parses a USN for releases affected", func(){
+	It("parses a USN for releases affected", func() {
 		usn := api.USN{URL: "https://ubuntu.com/security/notices/USN-4653-1"}
 		Expect(usn.IsForRelease("xenial")).To(BeTrue())
 	})
