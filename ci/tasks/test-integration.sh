66522083698cefce589e2078c7f99879469ea157
aram price <aram.price@broadcom.com>
2024-06-17 15:53:46 -0700

    Cleanup CI bash scripts.
    We do not believe that sourcing `root`'s `.bashrc` provides any value to
    running Ginkgo.
    
    Signed-off-by: Kenneth Lakin <kenneth.lakin@broadcom.com>

--- ci/tasks/test-integration.sh
+++ ci/tasks/test-integration.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-
 set -ex
-
-source ~/.bashrc
 
 cd usn-resource
 
-echo -e "\n Running tests..."
 go run github.com/onsi/ginkgo/v2/ginkgo --keep-going --trace --race -vv -r $@
